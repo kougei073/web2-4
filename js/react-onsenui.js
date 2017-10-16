d98b9654d540ad888b91f01dc43e00515422be51
kougei073 <g1627073@st.t-kougei.ac.jp>
2017-10-16 18:06:52 +0900

    Update react-onsenui.js

--- js/react-onsenui.js
+++ js/react-onsenui.js
@@ -4787,4 +4787,17 @@ exports.ToolbarButton = ToolbarButton;
 Object.defineProperty(exports, '__esModule', { value: true });
 
 })));
+
+document.addEventListener('init', function(event) {
+  var page = event.target;
+
+  if (page.id === 'page1') {
+    page.querySelector('#push-button').onclick = function() {
+      document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Page 2'}});
+    };
+  } else if (page.id === 'page2') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  }
+});
+
 //# sourceMappingURL=react-onsenui.js.map
